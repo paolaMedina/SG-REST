2462e4dc96fe17e38c882f577062066e6235f632
Paola <Paola@Paola-PC>
2017-05-24 23:16:00 -0500

    reporte de cuenta, terminado, sin probar

--- src/Reportes/ReporteCuenta.java
+++ src/Reportes/ReporteCuenta.java
@@ -8,6 +8,9 @@ package Reportes;
 import Clases.Pedido;
 import Clases.Producto;
 import Clases.ProductoPedido;
+import Controladores.PedidoJpaController;
+import Controladores.ProductoJpaController;
+import Controladores.ProductoPedidoJpaController;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
@@ -22,20 +25,23 @@ import java.text.DecimalFormat;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author Paola
  */
 public class ReporteCuenta {
-    
-    public void generarCuenta(List<ProductoPedido> productos,String numCuenta,Pedido pedido) {
+
+    public void generarCuenta(List<ProductoPedido> productos, String numCuenta, Pedido pedido) {
         String numeroDelPedido = pedido.getNumPedido().toString();
         String numMesa = pedido.getNumMesa().getMesa().toString();
         String empleadoQueAtendio = pedido.getIdEmpleado().getNombre().toString();
         String estadoPedido = pedido.getIdEstadoPedido().getEstado().toString();
         String horaInicioPedido = pedido.getHoraInicioPedido().toString();
         String horaFinalPedido = pedido.getHoraFinalPedido().toString();
+        long totalPagar=0;
 
         DecimalFormat formatea = new DecimalFormat("###,###.##");
         try {
@@ -49,8 +55,6 @@ public class ReporteCuenta {
             Paragraph titulo = new Paragraph("+------------------------------------------+Cuenta+------------------------------------------+");
             titulo.setAlignment(Element.ALIGN_CENTER);
             doc.add(titulo);
-            
-            
 
             doc.add(new Paragraph("Empresa: Crepes & Waffles"));
             doc.add(new Paragraph("Numéro de cuenta: " + numCuenta));
@@ -59,17 +63,51 @@ public class ReporteCuenta {
             doc.add(new Paragraph("Nombre del mesero " + empleadoQueAtendio));
             doc.add(new Paragraph("Hora de inicio del pedido: " + horaInicioPedido));
             doc.add(new Paragraph("Hora de fin del pedido: " + horaFinalPedido + "\n" + "\n" + "\n"));
-            
+
             PdfPTable table = new PdfPTable(2);
+            table.addCell("Producto");
+            table.addCell("Cantidad");
+            table.addCell("Precio");
+            for (int i = 0; i < productos.size(); i++) {
+
+                String nombreProducto = productos.get(i).getProductoPedidoPK().getProductoNombre();
+                int cantidadProducto = productos.get(i).getCatidad();
+                Long precio = valorProducto(nombreProducto, cantidadProducto);
+
+                table.addCell("" + nombreProducto + "");
+                table.addCell("" + cantidadProducto + "");
+                table.addCell(formatea.format(precio));
+                
+                totalPagar += precio;
+
+            }
             
+            doc.add(table);
+            doc.add(new Paragraph("Total a pagar: " + totalPagar + "\n" + "\n" + "\n"));
             
-            
+            doc.close(); 
+
         } catch (DocumentException ex) {
             Logger.getLogger(ReporteCuenta.class.getName()).log(Level.SEVERE, null, ex);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ReporteCuenta.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+    }
+
+    public Long valorProducto(String nombreProducto, int cantidadProducto) {
+        //Se crea en EntityManagerFactory con el nombre de nuestra unidad de persistencia
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SG-RESTPU");
+
+        //Se crean los controladores que se usaran en el metodo
+        ProductoPedidoJpaController daoProductoPedido = new ProductoPedidoJpaController(emf);
+        PedidoJpaController daoPedido = new PedidoJpaController(emf);
+        ProductoJpaController daoProducto = new ProductoJpaController(emf);
+        List<Producto> consulta = daoProducto.findProducto(nombreProducto);
+        Long precioUnitario = consulta.get(0).getPrecio();
+        Long precioProductos = precioUnitario * cantidadProducto;
+
+        return precioProductos;
     }
 
 }
